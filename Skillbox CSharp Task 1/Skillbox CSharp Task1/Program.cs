6f41fc653586e4518b4ec5c637e9698dec55d7a5
SidSherman <redneckchan@yandex.ru>
2022-09-07 11:05:29 +1200

    Added the first tasks solution

---
+++ Skillbox CSharp Task 1/Skillbox CSharp Task1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace skillbox_homework
+{
+    class SkillboxTask1
+    {
+        static void Main()
+        {
+            // 1.Объявление переменных
+
+            string fullName;
+            string email;
+            int age;
+            float codeScore;
+            float mathScore;
+            float physScore;
+
+            // 2. Присваивание значений переменным
+
+            fullName = "Laskus Aleksandr Sergeevich";
+            email = "myemail@konoha.com";
+            age = 23;
+            codeScore = 100f;
+            mathScore = 50f;
+            physScore = 100f;
+
+            // 3. Вывод значений переменных на экран
+
+            Console.WriteLine(
+                $"Ф.И.О: {fullName} \n" +
+                $"Возраст: {age} \n" +
+                $"Баллы по программированию: {codeScore} \n" +
+                $"Баллы по математике: {mathScore} \n" +
+                $"Баллы по физике: {physScore} \n" +
+                $"E-mail: {email}"
+                );
+
+            // 4. Подсчет суммы баллов с помощью функции Sum и вывод на экран
+
+            Console.WriteLine("Сумма всех баллов: " + Sum(new float[3] { codeScore, mathScore, physScore }));
+
+            // 5. Подсчет среднего арифметического баллов с помощью функции AverangeValue и вывод на экран
+
+            Console.WriteLine("Среднее арифметическое баллов: " + AverangeValue(new float[3] { codeScore, mathScore, physScore }));
+
+        }
+
+
+        // Функция для подсчета суммы значений
+        static float Sum(float[] values)
+        {
+            if (values.Length == 0)
+                return 0;
+
+            float sum = 0;
+
+            foreach (float value in values)
+            {
+                sum += value;
+            }
+
+            return sum;
+
+        }
+
+        // Функция для подсчета среднего арифметического
+        static float AverangeValue(float[] values)
+        {
+            if (values.Length == 0)
+                return 0;
+
+            float averangeValue = 0;
+
+            averangeValue = Sum(values) / values.Length;
+
+            return averangeValue;
+        }
+
+    }
+
+}
